745dcf67bc61e69109aab0027d76cec6691e81c9
bhaskarpenmetsa <45040979+bhaskarpenmetsa@users.noreply.github.com>
2023-02-13 11:00:25 +0000

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -1,7 +1,6 @@
 env.SAST_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.Deployment="http://43.204.5.162:8084/"
-env.WAR_FILE="SpringMVCAnnotationShoppingCart.war"
 env.DAST_URL="http://43.204.5.162:8084/SpringMVCAnnotationShoppingCart"
 env.FUNCTIONAL_WEB_GIT_URL="https://github.com/harishpallapu/NuReCampus.git -b master"
 env.FUNCTIONAL_MOBILE_GIT_URL="https://github.com/harishpallapu/mobiletest.git"
