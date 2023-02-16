728ddce41c1566bf0a102b86b7dddb4b41f0d77f
sanagalavamsi <sanagala.vamsikiran@3i-infotech.com>
2023-02-16 15:00:41 +0530

    Added Groovy File Parameters

--- parameters.groovy
+++ parameters.groovy
@@ -1,8 +1,7 @@
 env.SAST_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
-env.Deployment="http://43.204.5.162:8084/"
-env.DAST_URL="http://43.204.5.162:8084/SpringMVCAnnotationShoppingCart"
-env.FUNCTIONAL_WEB_GIT_URL="https://github.com/harishpallapu/NuReCampus.git -b master"
+env.TOMCAT_IP="https://github.com/harishpallapu/NuReCampus.git -b master"
+env.FUNCTIONAL_WEB_GIT_URL=
 env.FUNCTIONAL_MOBILE_GIT_URL="https://github.com/harishpallapu/mobiletest.git"
 env.JMETER_GIT_URL="https://github.com/harishpallapu/folder.git"
-env.EMAIL_ID="flexib08@gmail.com"
+env.NEXUS_IP="flexib08@gmail.com"
