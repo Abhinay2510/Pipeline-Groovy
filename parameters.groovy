0ed3526afcd12af67e9065d92dec1cc0b0626e32
bhaskarpenmetsa <bhaskar88.p@gmail.com>
2023-05-03 23:04:52 -0700

    Added Groovy File Parameters

--- parameters.groovy
+++ parameters.groovy
@@ -1,9 +1,5 @@
-env.SAST_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
+env.SAST_GIT_URL=
 env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
-env.Deployment="http://localhost:8008/"
-env.DAST_URL="http://10.1.127.121:8008/SpringMVCAnnotationShoppingCart/"
-env.FUNCTIONAL_WEB_GIT_URL="https://github.com/bhaskarpenmetsa/web_auto.git"
-env.FUNCTIONAL_MOBILE_GIT_URL="https://github.com/harishpallapu/mobiletest.git"
-env.JMETER_GIT_URL="https://github.com/harishpallapu/folder.git"
-env.EMAIL_ID="flexib08@gmail.com"
-
+env.FUNCTIONAL_WEB_GIT_URL=
+env.FUNCTIONAL_MOBILE_GIT_URL=
+env.JMETER_GIT_URL=
